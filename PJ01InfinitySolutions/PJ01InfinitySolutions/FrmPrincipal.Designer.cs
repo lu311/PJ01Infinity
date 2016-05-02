52431fd60a92c4b4026cc976759effe8d4e269ff
Luciano Andrade <lu311@outlook.com>
2016-05-02 16:11:25 -0300

    tets

--- PJ01InfinitySolutions/PJ01InfinitySolutions/FrmPrincipal.Designer.cs
+++ PJ01InfinitySolutions/PJ01InfinitySolutions/FrmPrincipal.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::PJ01InfinitySolutions.Properties.Resources.user1_add;
+            this.button1.Location = new System.Drawing.Point(162, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 93);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 331);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmPrincipal";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
