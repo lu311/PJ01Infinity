6046c5252f103a30a2212a7b3049259b9478e88f
Luciano <lu311@outlook.com>
2016-05-14 10:27:38 -0300

    edson

---
+++ PJ01InfinitySolutions/PJ01Model/PessoaCategoriaDados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJ01Model
+{
+    class PessoaCategoriaDados
+    {
+    }
+}
